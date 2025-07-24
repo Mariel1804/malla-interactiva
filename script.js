e6b4e8c34d5233941bb07966abb28a8c35769a0e
Mariel1804 <marielguerraa.18@gmail.com>
2025-07-24 01:26:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.querySelectorAll('.ramo').forEach(ramo => {
+  ramo.addEventListener('click',()=> {
+    const siguiente = ramo.dataset.desbloquea;
+    if (siguiente) {
+      const desbloqueado = document.getElementaryById(siguiente);
+      if (desbloqueado && desbloqueado.classList.contains('bloqueado')) {
+        desbloqueado.classList.remove('bloqueado');
+      }
+    }
+  });
+});
